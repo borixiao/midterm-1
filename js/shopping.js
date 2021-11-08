c603f3813128e193893820222d5a0f8ca3546cf0
boruxiao <ntue19037@gmail.com>
2021-11-08 20:29:03 +0800

    boruxiao-new-version

--- js/shopping.js
+++ js/shopping.js
@@ -1,15 +1,23 @@
 $(".left-toggle").click(function () {
     $(".sublist").toggleClass("active");
+    $(".dis").toggleClass("displaynone");
+    $(".dis1").toggleClass("active1");
 });
 
 $(".left-toggle2").click(function () {
     $(".sublist2").toggleClass("active");
+    $(".dis-2").toggleClass("displaynone");
+    $(".dis2").toggleClass("active1");
 });
 
 $(".left-toggle3").click(function () {
     $(".sublist3").toggleClass("active");
+    $(".dis-3").toggleClass("displaynone");
+    $(".dis3").toggleClass("active1");
 });
 
 $(".left-toggle4").click(function () {
     $(".sublist4").toggleClass("active");
+    $(".dis-4").toggleClass("displaynone");
+    $(".dis4").toggleClass("active1");
 });
