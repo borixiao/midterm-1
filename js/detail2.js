357c3a81143051a3a6c9d0c493ed5b07d160bc3d
boruxiao <ntue19037@gmail.com>
2022-01-01 16:01:12 +0800

    2022-1-1 version

---
+++ js/detail2.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+
+    // Initialize Firebase
+    firebase.initializeApp({
+        apiKey: "AIzaSyBTz55pYp87GQX4u3tU32Z6J2xiD0v6ZIA",
+        authDomain: "web202211.firebaseapp.com",
+        projectId: "web202211",
+        storageBucket: "web202211.appspot.com",
+        messagingSenderId: "1064218922449",
+        appId: "1:1064218922449:web:0f8cfa0089a40c8e5024f0",
+        measurementId: "G-M5V70Q5X5W"
+    });
+    // REFERENCE TO FIREBASE AUTH
+    const auth = firebase.auth();
+
+    // STORE PROFILE INFO
+    let profile_Name,
+        profile_photoURL;
+
+    // REGISTER DOM ELEMENTS
+    const $email = $('#email');
+    const $password = $('#password');
+    const $btnSignIn = $('#btnSignIn');
+    const $btnSignUp = $('#btnSignUp');
+    const $btnSignOut = $('#btnSignOut');
+    const $signInfo = $('#sign-info');
+    const avatarImage = $('.avatar-image');
+    const avatarName = $('.avatar-name');
+    const avatarEmail = $('.avatar-email');
+    $signInfo.html("");
+
+    // SignIn
+    $btnSignIn.click(function (e) {
+        $btnSignIn.html(`<span class="spinner-border spinner-border-sm"></span>`);
+        auth.signInWithEmailAndPassword($email.val(), $password.val())
+            .then(function (e) {
+                $btnSignIn.html(`Sign In`);
+                window.location.href = "./Profile.html";
+            })
+            .catch(function (e) {
+                $btnSignIn.html(`Sign In`);
+                console.log(e.message);
+                $signInfo.html(e.message);
+            });
+    });
+
+    // SignUp
+    $btnSignUp.click(function (e) {
+        console.log('sign up now ...');
+        $btnSignUp.html(`<span class="spinner-border spinner-border-sm"></span>`);
+        auth.createUserWithEmailAndPassword($email.val(), $password.val())
+            .then(function () {
+                const user = auth.currentUser;
+                user.updateProfile({
+                    displayName: $('#userName').val(),
+                    photoURL: $('#photoURL').val()
+                })
+                    .then(function () {
+                        $btnSignUp.html(`Sign Up`);
+                        $email.val('');
+                        $password.val('');
+                        $('#userName').val('');
+                        $('#photoURL').val('');
+                        window.location.href = "./Profile.html";
+                    });
+            })
+            .catch(function (e) {
+                $btnSignUp.html(`Sign Up`);
+                $signInfo.html(e.message);
+            });
+    });
+
+    // Listening Login User
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            console.log(user);
+            $signInfo.html(`${user.email} is login...`);
+            user.providerData.forEach(function (profile) {
+                console.log(`  Sign-in provider: ${profile.providerId}`);
+                console.log(`  Provider-specific UID: ${profile.uid}`);
+                console.log(`  Name: ${profile.displayName}`);
+                console.log(`  Email: ${profile.email}`);
+                console.log(`  Photo URL: ${profile.photoURL}`);
+                profile_Name = profile.displayName;
+                profile_photoURL = profile.email;
+                avatarName.html(profile.displayName);
+                avatarEmail.html(profile.email);
+                avatarImage.attr("src", profile.photoURL);
+            });
+        } else {
+            console.log("not logged in");
+        }
+    });
+
+
+    // Signout
+    $btnSignOut.click(function () {
+        auth.signOut();
+        $email.val('');
+        $password.val('');
+        $signInfo.html('No one login...');
+        window.location.href = "./Signin.html";
+    });
+});
--- js/details.js
+++ js/details.js
@@ -1,25 +1,124 @@
-$(document).ready(function () {
+new WOW().init();
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling: true,
+    scrollHorizontally: true
+});
+// $(document).ready(function () {
 
-    // Register GSAP ScrollTrigger Plugin
-    // gsap.registerPlugin(ScrollTrigger);
+//     // Register GSAP ScrollTrigger Plugin
+//     // gsap.registerPlugin(ScrollTrigger);
 
-    // set GSAP Timeline
-    let timeLine = new gsap.timeline({
-        scrollTrigger: {
-            trigger: "#block",//觸發點設置(要設誰為觸發點(本例子是方塊本身)，當方塊的正中央碰到畫面的正中央就開始播動畫)
-            pin: true, // pin the trigger element while active
-            start: "center center", // when the center of the trigger hits the center of the viewport
-            end: "+=500", // end after scrolling 500px beyond the start
-            scrub: true,
-            markers: true,
-            id: "block",
-        },
-    });
-    timeLine
-        .to("#block", 2, { backgroundColor: "red" })
-        .to("#block", 1, {
-            x: $(window).width() - $("#block").width(),
-        }, 0)
-        .to("#block", 1, { x: 0 })
-    // 給0是兩個動畫一起播，沒有就是依序播
-})
+//     // set GSAP Timeline
+//     let timeLine = new gsap.timeline({
+//         scrollTrigger: {
+//             trigger: "#block",//觸發點設置(要設誰為觸發點(本例子是方塊本身)，當方塊的正中央碰到畫面的正中央就開始播動畫)
+//             pin: true, // pin the trigger element while active
+//             start: "center center", // when the center of the trigger hits the center of the viewport
+//             end: "+=500", // end after scrolling 500px beyond the start
+//             scrub: true,
+//             markers: true,
+//             id: "block",
+//         },
+//     });
+//     timeLine
+//         .to("#block", 2, { backgroundColor: "red" })
+//         .to("#block", 1, {
+//             x: $(window).width() - $("#block").width(),
+//         }, 0)
+//         .to("#block", 1, { x: 0 })
+//     // 給0是兩個動畫一起播，沒有就是依序播
+// })
+// $(document).ready(function () {
+//     // Register GSAP ScrollTrigger Plugin
+//     gsap.registerPlugin(ScrollTrigger);
+//     // lottie開始
+//     let openingAnimWindow = document.querySelector("#openingLottie");
+//     let openingAnimData = {
+//         container: openingAnimWindow,
+//         animType: "svg",
+//         loop: false,
+//         prerender: true,
+//         autoplay: false,
+//         path: 'https://assets8.lottiefiles.com/packages/lf20_aDPqr7.json'
+//     };
+//     // set bodymovin
+//     let scrollAnim = bodymovin.loadAnimation(openingAnimData);
+//     // Set Pin Page 設定一個畫面的鎖定pin，開始點是top top，意思是當物件的top碰觸到畫面的top就開始播
+//     ScrollTrigger.create({
+//         trigger: "#page1",
+//         start: "top top",
+//         pin: true,
+//         pinSpacing: false,
+//     });
+
+//     // Set GSAP Timeline with Pin
+//     let timeLine_page2 = new gsap.timeline({
+//         scrollTrigger: {
+//             trigger: "#page2",
+//             yoyo: true,
+//             pin: true, // pin the trigger element while active
+//             start: "bottom bottom", // when the top of the trigger hits the top of the viewport
+//             end: "100%", // end after scrolling 500px beyond the start
+//             scrub: true,
+//             markers: true,
+//             id: "page2",
+//         },
+//     });
+//     scrollAnim.addEventListener("DOMLoaded", onDOMLoaded);
+
+//     // // Frame-by-frame play
+//     function onDOMLoaded() {
+//         timeLine_page2.to(
+//             {
+//                 frame: 0,
+//             },
+//             3,
+//             {
+//                 frame: scrollAnim.totalFrames - 1,
+//                 onUpdate: function () {
+//                     scrollAnim.goToAndStop(Math.round(this.targets()[0].frame), true);
+//                     $("#counter").html(Math.round(this.targets()[0].frame));
+//                 },
+//                 ease: Linear.easeNone,
+//             }
+//         );
+
+//     } //不管她的變數名稱是甚麼，反正他就是要播下面這個動畫，只是觸發的東西變了，變得不是物件本身
+//     // .to("#block", 2, { backgroundColor: "red" })
+//     // .to("#block", 1, { x: $(window).width() - $("#block").width() }, 0)
+//     // .to("#block", 1, { x: 0 });
+//     // .fromTo("#block", 1, { autoAlpha: 0 }, { autoAlpha: 1 });
+
+
+//     let timeLine_page3 = new gsap.timeline({
+//         scrollTrigger: {
+//             trigger: "#page3",
+//             yoyo: true,
+//             pin: true, // pin the trigger element while active
+//             start: "bottom bottom", // when the top of the trigger hits the top of the viewport
+//             end: "100%", // end after scrolling 500px beyond the start
+//             scrub: true,
+//             markers: true,
+//             id: "page3",
+//         },
+//     });
+//     timeLine_page3.to(
+//         {
+//             frame: 0,
+//         },
+//         3,
+//         {
+//             frame: scrollAnim.totalFrames - 1,
+//             onUpdate: function () {
+//                 scrollAnim.goToAndStop(Math.round(this.targets()[0].frame), true);
+//                 $("#counter").html(Math.round(this.targets()[0].frame));
+//             },
+//             ease: Linear.easeNone,
+//         }
+//     );
+
+
+
+// });
+
