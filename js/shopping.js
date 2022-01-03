d7a29cc99c77dde3ceb3ba233f5a43a8d6c2a7ed
boruxiao <ntue19037@gmail.com>
2022-01-03 23:27:37 +0800

    202213final

--- js/shopping.js
+++ js/shopping.js
@@ -1,23 +1,23 @@
 $(".left-toggle").click(function () {
-    $(".sublist").toggleClass("active");
+    $(".sublist").slideToggle("active");
     $(".dis").toggleClass("displaynone");
     $(".dis1").toggleClass("active1");
 });
 
 $(".left-toggle2").click(function () {
-    $(".sublist2").toggleClass("active");
+    $(".sublist2").slideToggle("active");
     $(".dis-2").toggleClass("displaynone");
     $(".dis2").toggleClass("active1");
 });
 
 $(".left-toggle3").click(function () {
-    $(".sublist3").toggleClass("active");
+    $(".sublist3").slideToggle("active");
     $(".dis-3").toggleClass("displaynone");
     $(".dis3").toggleClass("active1");
 });
 
 $(".left-toggle4").click(function () {
-    $(".sublist4").toggleClass("active");
+    $(".sublist4").slideToggle("active");
     $(".dis-4").toggleClass("displaynone");
     $(".dis4").toggleClass("active1");
 });
