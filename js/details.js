2706d0891102c2f7153fc7b46fa8814d1961b28c
boruxiao <ntue19037@gmail.com>
2022-01-09 21:49:55 +0800

    202219

--- js/details.js
+++ js/details.js
@@ -1,9 +1,9 @@
 new WOW().init();
-new fullpage('#fullpage', {
-    //options here
-    autoScrolling: true,
-    scrollHorizontally: true
-});
+// new fullpage('#fullpage', {
+//     //options here
+//     autoScrolling: true,
+//     scrollHorizontally: true
+// });
 // $(document).ready(function () {
 
 //     // Register GSAP ScrollTrigger Plugin
@@ -30,96 +30,163 @@ new fullpage('#fullpage', {
 //     // 給0是兩個動畫一起播，沒有就是依序播
 // })
 
-// $(document).ready(function () {
-//     // Register GSAP ScrollTrigger Plugin
-//     gsap.registerPlugin(ScrollTrigger);
-//     // lottie開始
-//     let openingAnimWindow = document.querySelector("#openingLottie");
-//     let openingAnimData = {
-//         container: openingAnimWindow,
-//         animType: "svg",
-//         loop: false,
-//         prerender: true,
-//         autoplay: false,
-//         path: 'https://assets8.lottiefiles.com/packages/lf20_aDPqr7.json'
-//     };
-//     // set bodymovin
-//     let scrollAnim = bodymovin.loadAnimation(openingAnimData);
-//     // Set Pin Page 設定一個畫面的鎖定pin，開始點是top top，意思是當物件的top碰觸到畫面的top就開始播
-//     ScrollTrigger.create({
-//         trigger: "#page1",
-//         start: "top top",
-//         pin: true,
-//         pinSpacing: false,
-//     });
+$(document).ready(function () {
+    // Register GSAP ScrollTrigger Plugin
+    gsap.registerPlugin(ScrollTrigger);
+    // lottie開始
+    let openingAnimWindow = document.querySelector("#openingLottie");
+    let openingAnimData = {
+        container: openingAnimWindow,
+        animType: "svg",
+        loop: false,
+        prerender: true,
+        autoplay: false,
+        path: 'https://assets8.lottiefiles.com/packages/lf20_aDPqr7.json'
+    };
+    // set bodymovin
+    let scrollAnim = bodymovin.loadAnimation(openingAnimData);
+    // Set Pin Page 設定一個畫面的鎖定pin，開始點是top top，意思是當物件的top碰觸到畫面的top就開始播
+    ScrollTrigger.create({
+        trigger: "#page1",
+        start: "top top",
+        pin: true,
+        pinSpacing: false,
+    });
 
-//     // Set GSAP Timeline with Pin
-//     let timeLine_page2 = new gsap.timeline({
-//         scrollTrigger: {
-//             trigger: "#page2",
-//             yoyo: true,
-//             pin: true, // pin the trigger element while active
-//             start: "bottom bottom", // when the top of the trigger hits the top of the viewport
-//             end: "100%", // end after scrolling 500px beyond the start
-//             scrub: true,
-//             markers: true,
-//             id: "page2",
-//         },
-//     });
-//     scrollAnim.addEventListener("DOMLoaded", onDOMLoaded);
+    // Set GSAP Timeline with Pin
+    let timeLine_page2 = new gsap.timeline({
+        scrollTrigger: {
+            trigger: "#page2",
+            yoyo: true,
+            pin: true, // pin the trigger element while active
+            start: "bottom bottom", // when the top of the trigger hits the top of the viewport
+            end: "100%", // end after scrolling 500px beyond the start
+            scrub: true,
+            markers: true,
+            id: "page2",
+        },
+    });
+    scrollAnim.addEventListener("DOMLoaded", onDOMLoaded);
 
-//     // // Frame-by-frame play
-//     function onDOMLoaded() {
-//         timeLine_page2.to(
-//             {
-//                 frame: 0,
-//             },
-//             3,
-//             {
-//                 frame: scrollAnim.totalFrames - 1,
-//                 onUpdate: function () {
-//                     scrollAnim.goToAndStop(Math.round(this.targets()[0].frame), true);
-//                     $("#counter").html(Math.round(this.targets()[0].frame));
-//                 },
-//                 ease: Linear.easeNone,
-//             }
-//         );
+    // // Frame-by-frame play
+    function onDOMLoaded() {
+        timeLine_page2.to(
+            {
+                frame: 0,
+            },
+            3,
+            {
+                frame: scrollAnim.totalFrames - 1,
+                onUpdate: function () {
+                    scrollAnim.goToAndStop(Math.round(this.targets()[0].frame), true);
+                    $("#counter").html(Math.round(this.targets()[0].frame));
+                },
+                ease: Linear.easeNone,
+            }
+        );
 
-//     } //不管她的變數名稱是甚麼，反正他就是要播下面這個動畫，只是觸發的東西變了，變得不是物件本身
-//     // .to("#block", 2, { backgroundColor: "red" })
-//     // .to("#block", 1, { x: $(window).width() - $("#block").width() }, 0)
-//     // .to("#block", 1, { x: 0 });
-//     // .fromTo("#block", 1, { autoAlpha: 0 }, { autoAlpha: 1 });
+    }
+
+    //page3
+
+    let openingAnimWindow1 = document.querySelector("#openingLottie1");
+    let openingAnimData1 = {
+        container: openingAnimWindow1,
+        animType: "svg",
+        loop: false,
+        prerender: true,
+        autoplay: false,
+        path: 'https://assets8.lottiefiles.com/packages/lf20_aDPqr7.json'
+    };
+    // set bodymovin
+    let scrollAnim1 = bodymovin.loadAnimation(openingAnimData1);
+    // Set Pin Page 設定一個畫面的鎖定pin，開始點是top top，意思是當物件的top碰觸到畫面的top就開始播
+    ScrollTrigger.create({
+        trigger: "#page3",
+        start: "top top",
+        pin: true,
+        pinSpacing: false,
+    });
+
+    // Set GSAP Timeline with Pin
+    let timeLine_page3 = new gsap.timeline({
+        scrollTrigger: {
+            trigger: "#page3",
+            yoyo: true,
+            pin: true, // pin the trigger element while active
+            start: "bottom bottom", // when the top of the trigger hits the top of the viewport
+            end: "100%", // end after scrolling 500px beyond the start
+            scrub: true,
+            markers: true,
+            id: "page3",
+        },
+    });
+    scrollAnim1.addEventListener("DOMLoaded", onDOMLoaded1);
+
+    // // Frame-by-frame play
+    function onDOMLoaded1() {
+        timeLine_page3.to(
+            {
+                frame: 0,
+            },
+            3,
+            {
+                frame: scrollAnim1.totalFrames - 1,
+                onUpdate: function () {
+                    scrollAnim1.goToAndStop(Math.round(this.targets()[0].frame), true);
+                    $("#counter").html(Math.round(this.targets()[0].frame));
+                },
+                ease: Linear.easeNone,
+            }
+        );
+
+    }
 
 
-//     let timeLine_page3 = new gsap.timeline({
-//         scrollTrigger: {
-//             trigger: "#page3",
-//             yoyo: true,
-//             pin: true, // pin the trigger element while active
-//             start: "bottom bottom", // when the top of the trigger hits the top of the viewport
-//             end: "100%", // end after scrolling 500px beyond the start
-//             scrub: true,
-//             markers: true,
-//             id: "page3",
-//         },
-//     });
-//     timeLine_page3.to(
-//         {
-//             frame: 0,
-//         },
-//         3,
-//         {
-//             frame: scrollAnim.totalFrames - 1,
-//             onUpdate: function () {
-//                 scrollAnim.goToAndStop(Math.round(this.targets()[0].frame), true);
-//                 $("#counter").html(Math.round(this.targets()[0].frame));
-//             },
-//             ease: Linear.easeNone,
-//         }
-//     );
+});
+
+//不管她的變數名稱是甚麼，反正他就是要播下面這個動畫，只是觸發的東西變了，變得不是物件本身
+    // .to("#block", 2, { backgroundColor: "red" })
+    // .to("#block", 1, { x: $(window).width() - $("#block").width() }, 0)
+    // .to("#block", 1, { x: 0 });
+    // .fromTo("#block", 1, { autoAlpha: 0 }, { autoAlpha: 1 });
+    // let openingAnimWindow1 = document.querySelector("#openingLottie1");
+
+    // let openingAnimData1 = {
+    //     container: openingAnimWindow1,
+    //     animType: "svg",
+    //     loop: false,
+    //     prerender: true,
+    //     autoplay: false,
+    //     path: 'https://assets8.lottiefiles.com/packages/lf20_aDPqr7.json'
+    // };
+    // let scrollAnim1 = bodymovin.loadAnimation(openingAnimData1);
+    // scrollAnim1.addEventListener("DOMLoaded", onDOMLoaded);
 
 
-
-// });
-
+    // let timeLine_page3 = new gsap.timeline({
+    //     scrollTrigger: {
+    //         trigger: "#page3",
+    //         yoyo: true,
+    //         pin: true, // pin the trigger element while active
+    //         start: "bottom bottom", // when the top of the trigger hits the top of the viewport
+    //         end: "100%", // end after scrolling 500px beyond the start
+    //         scrub: true,
+    //         markers: true,
+    //         id: "page3",
+    //     },
+    // });
+    // timeLine_page3.to(
+    //     {
+    //         frame: 0,
+    //     },
+    //     3,
+    //     {
+    //         frame: scrollAnim.totalFrames - 1,
+    //         onUpdate: function () {
+    //             scrollAnim.goToAndStop(Math.round(this.targets()[0].frame), true);
+    //             $("#counter").html(Math.round(this.targets()[0].frame));
+    //         },
+    //         ease: Linear.easeNone,
+    //     }
+    // );
